00da4ac5e577568b18ff2e4465b4bf8e82cecdb6
sharkiecodes <sharkiecodes@gmail.com>
2023-07-12 11:41:17 +0200

    layout changes

--- app/src/main/java/com/sharkie/repscalculator/MainActivity.java
+++ app/src/main/java/com/sharkie/repscalculator/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 
@@ -15,46 +16,18 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     //Defining da buttons
-    Button qrbutton, workoutButton, weightButton;
+    ImageButton workoutButton, weightButton;
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        workoutButton = (Button) findViewById(R.id.workoutButton);
-        weightButton = (Button) findViewById(R.id.weightButton);
-        qrbutton = (Button) findViewById(R.id.qrbutton);
-
-
-        qrbutton.setOnClickListener(view -> {
-            // inflate the layout of the popup window
-            LayoutInflater inflater = (LayoutInflater)
-                    getSystemService(LAYOUT_INFLATER_SERVICE);
-            @SuppressLint("InflateParams") View popupView = inflater.inflate(R.layout.qrcode, null);
-
-            // create the popup window
-            int width = LinearLayout.LayoutParams.WRAP_CONTENT;
-            int height = LinearLayout.LayoutParams.WRAP_CONTENT;
-            boolean focusable = true; // lets taps outside the popup also dismiss it
-            final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
-
-            // show the popup window
-            // which view you pass in doesn't matter, it is only used for the window tolken
-            popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
-
-            // dismiss the popup window when touched
-            popupView.setOnTouchListener((v, event) -> {
-                popupWindow.dismiss();
-                return true;
-
-
-    });
+        workoutButton = (ImageButton) findViewById(R.id.workoutButton);
+        weightButton = (ImageButton) findViewById(R.id.weightButton);
 
 
 
-
-});
 
         workoutButton.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, workout.class)));
 
